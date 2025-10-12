62a2f139913ad585ae6f864cf6df266542cc6651
SanskrutiSakharkar <sanskrutissakharkar@gmail.com>
2025-10-12 16:17:04 +1300

    Updated Files

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ connectDB();
 // CORS middleware (adjust origin if needed)
 // server.js
 app.use(cors({
-  origin: ['http://98.88.37.110:3000'],
+  origin: ['http://52.207.242.133:3000'],
   credentials: false
 }));
 
